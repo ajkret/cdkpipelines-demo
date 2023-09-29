ea747ab63ef34a4aaa1953d60032744b482f1b39
Adriano Kretschmer <adrianok_ext@kpit.com>
2023-09-29 17:46:30 -0300

    Initial commit

---
+++ lib/pipelines-stack.ts
@@ -0,0 +1,11 @@
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class PipelinesStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    
+
+  }
+}
