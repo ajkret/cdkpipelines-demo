7ae5e11fd77a6feafe0cead250299953b15b964a
Adriano Kretschmer <adrianok_ext@kpit.com>
2023-09-29 18:21:18 -0300

    Added new stage

--- bin/pipelines.ts
+++ bin/pipelines.ts
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { PipelinesStack } from '../lib/pipelines-stack';
+import { CdkpipelinesDemoPipelineStack } from '../lib/pipelines-demo-pipeline-stack';
+
+const stackProps = {
+    // Dev Note: this removes an annoyying buggy message regarding Bootstraping the environment
+    synthesizer: new cdk.DefaultStackSynthesizer({
+        generateBootstrapVersionRule: false,
+    })
+};
 
 const app = new cdk.App();
-new PipelinesStack(app, 'PipelinesStack');
+new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', stackProps);
